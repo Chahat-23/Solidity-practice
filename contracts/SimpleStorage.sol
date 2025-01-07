15cffec71f8eef530fddf422b6e4fb793741bafd
Chahat-23 <chahatshrivastava230620@gmail.com>
2025-01-08 00:09:48 +0530

    'simple-storage'

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract SimpleStorage {
+    uint public favNum;
+
+    function store(uint favoriteNum) public {
+        favNum = favoriteNum;
+    }
+}
+
