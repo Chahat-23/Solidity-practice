30b4706a2c13fd70d4689ba40ce8a4314aadb763
Chahat-23 <chahatshrivastava230620@gmail.com>
2025-01-11 19:44:40 +0530

    'import contracts'

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -4,7 +4,10 @@ import "./SimpleStorage.sol";
 
 contract StorageFactory {
     SimpleStorage public simpleStorage;
+    SimpleStorage[] public simpleStorageArray;
+
     function createSimpleSTorageContract () public {
         simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
     }
 }
